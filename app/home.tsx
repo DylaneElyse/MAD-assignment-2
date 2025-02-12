b0b538fcafec3f45cbdf39065c2dd5f41329c24a
Rodrigo Rangel <130096450+Rodrigo1Rangel@users.noreply.github.com>
2025-02-11 21:44:06 -0700

    Remove comments

--- app/home.tsx
+++ app/home.tsx
@@ -7,23 +7,16 @@ import City from "./city-page";
 import {Calgary} from "../components/cities";
 import {Edmonton} from "../components/cities";
 import Splash from "./splash";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+
 
 export default function Home() {
     const Tab = createBottomTabNavigator();
-    // const cityName = "Calgary";
-    // const image = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Calgary_downtown_skyline.jpg/1200px-Calgary_downtown_skyline.jpg";
-    // const website = "https://www.visitcalgary.com/";
-
     return(
-    //<View>
-        //<Text>Welcome to my App</Text>
-        //<NavigationContainer>
             <Tab.Navigator>
-                <Tab.Screen name="Home" options={{ title: "Home" }} component={() => <Splash/>} />
-                <Tab.Screen name="Calgary" options={{ title: "Calgary" }} component={() => <Calgary/>} />
-                <Tab.Screen name="Edmonton" options={{ title: "Edmonton" }} component={() => <Edmonton/>} />
+                <Tab.Screen name="Home" options={{ title: "Home", tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} /> }} component={() => <Splash/>} />
+                <Tab.Screen name="Calgary" options={{ title: "Calgary", tabBarIcon: ({ color }) => <FontAwesome size={28} name="bookmark" color={color} /> }} component={() => <Calgary/>} />
+                <Tab.Screen name="Edmonton" options={{ title: "Edmonton", tabBarIcon: ({ color }) => <FontAwesome size={28} name="bookmark" color={color} /> }} component={() => <Edmonton/>} />
             </Tab.Navigator>
-        //</NavigationContainer>
-    //</View>
     )
 };
