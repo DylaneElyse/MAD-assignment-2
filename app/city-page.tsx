d4c345172682f19a7b49f3a467ed43c8358cc01c
Rodrigo Rangel <130096450+Rodrigo1Rangel@users.noreply.github.com>
2025-02-11 21:54:13 -0700

    Update city-page.tsx

--- app/city-page.tsx
+++ app/city-page.tsx
@@ -10,9 +10,6 @@ export default function City({ cityName, image, website}: CityProps) {
     return (
         <View>
             <Image style = {{height: 300, width: '100%'}} source={image} />
-
-            {/* <Image style = {{height: 100}} source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/b/bb/Downtown_Calgary_2020-4.jpg' }}/> */}
-            {/* <Image style = {{height: 100}} source={{ uri: image }}/> */}
             
             <Text>{cityName}</Text>
             <Text onPress={() => Linking.openURL(website)}>Visit their website here</Text>
